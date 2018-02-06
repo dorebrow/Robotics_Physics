6ff7d9bdcb91ae147a498e532b4599091373795d
dorebrow <dorebrow@ut.utm.edu>
2018-02-06 08:59:02 -0600

    Create motor_drive.ino

---
+++ motor_drive.ino
@@ -0,0 +1,29 @@
+const int forward = 7;
+const int backward = 6;
+
+long duration;
+int distance;
+
+void setup() {
+  //pinMode(trigPin, OUTPUT); //sets trigPin as Output
+  //pinMode(echoPin, INPUT); //sets echoPin as Input
+  //Serial.begin(9600); //starts serial communication
+}
+
+void loop () {
+  //digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  //digitalWrite(trigPin, 0);
+   
+  //digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  //digitalWrite(trigPin, LOW);
+
+ //duration = pulseIn(echoPin, HIGH);
+
+ //distance = duration*0.034/2;
+ //Serial.print("Distance: ");
+ //Serial.print(distance);
+ //Serial.print(" cm ");
+ //Serial.print("\n");
+}
